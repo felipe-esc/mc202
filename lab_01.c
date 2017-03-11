4feed806d6d937177f3fbc7bfbf37d1b80d59a7a
felipe-esc <felipe.e2742@gmail.com>
2017-03-11 16:31:25 -0300

    Pequenas mudanças em alguns detalhes
    alguns pequenos detalhes, nenhuma adição ao algoritmo ainda

--- lab_01.c
+++ lab_01.c
@@ -1,4 +1,5 @@
 /* Nome: Felipe Escórcio de Sousa	RA: 171043
+ * Nome:                          RA: 
  * Programa: Jogo de paciência
  * Descrição: O programa implementa uma partida de paciência, com as regras clássicas:
 	- Usa-se um baralho com 52 cartas
@@ -22,8 +23,7 @@
 #include <stdlib.h>
 
 typedef struct baralho{
-	char valor[2];	//guarda valor e naipe.
-	int face;	//índice 0 marca uma carta não virada, 1 uma já virada(aberta).
+	char valor[3];	//guarda valor, naipe e um índice(0 marca uma carta não virada, 1 uma já virada(aberta)).
 	struct carta *acima;
 }carta;
 
@@ -34,13 +34,13 @@ int main(void){
 	carta *S1 = NULL, *S2 = NULL, *S3 = NULL, *S4 = NULL, /*O-Ouros , C-Copas, P-Paus, E-Espadas */
 		*J1 = NULL, *J2 = NULL, *J3 = NULL, *J4 = NULL, *J5 = NULL, *J6 = NULL, *J7 = NULL,
 		*D = NULL, *E = NULL;
-	
-	
+
+
 
 	return 0;
 }
 
-/* Distribui as primeiras 28 cartas nas 7 pilhas de jogo. 
+/* Distribui as primeiras 28 cartas nas 7 pilhas de jogo.
  * Pega as primeiras 28 cartas, guarda-as num vetor e depois as passa para as filas, sendo que de uma carta pra outra há uma diferença de posição de inicialmente 5 números (com exceção da primeira pilha em que há apenas uma carta) e decresce conforme as camadas aumentam até a diferença ser de 1 posição apenas.
  */
 void dist_inicial(carta **P1){
@@ -48,12 +48,19 @@ void dist_inicial(carta **P1){
 	char **iniciais;
 	int i = 0;
 
-	**iniciais = *(char) malloc(sizeof(char) * 28);
+	**iniciais = (char*) malloc(sizeof(char) * 28);
 	for(i = 0; i < 28; i++){
 		*iniciais[i] = (char) malloc(sizeof(char) * 2);
+		/* iniciais[i] = getchar(stdin);
+		iniciais[i][3] = 0 */
 	}
-	
-	
+
+
+}
+/* Cria lista da pilha de estoque. */
+carta *criaEstoque(){
+
+	return estoque;
 }
 /* Pega todas as cartas e guarda na caixa. */
 void guarda(carta **P1){
